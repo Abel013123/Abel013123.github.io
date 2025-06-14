4061845c394c7a6ecd57a187b160bbb1468d7a6e
Abel013123 <ronnierivera92180122@gmail.com>
2025-06-14 20:51:18 +0800

    Site updated: 2025-06-14 20:51:17

--- anzhiyu/random.js
+++ anzhiyu/random.js
@@ -1,3 +1,3 @@
-var posts=["2025/06/14/test/"];function toRandomPost(){
+var posts=["2025/06/14/NongGang/","2025/06/14/test/"];function toRandomPost(){
     pjax.loadUrl('/'+posts[Math.floor(Math.random() * posts.length)]);
   };
